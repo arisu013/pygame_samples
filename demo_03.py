55a756d3de73f54f4b9103c050f4bfe1bfc553c1
Arisu44678214 <108202011+arisu013@users.noreply.github.com>
2024-03-02 21:14:27 +0900

    update demo_03.py

--- demo_03.py
+++ demo_03.py
@@ -16,31 +16,9 @@ WHITE = (250, 250, 250)
 
 pygame.init()
 
-clock = pygame.time.Clock()
-screen = pygame.display.set_mode([400, 320])
-pygame.display.set_caption("pygame 7-segment display simulation")
-screen.fill(DARK_GRAY)
 
-display1 = LCD_font(screen)
-display1.init_col(BLOCK_SIZE=9, BLOCK_INTV=10, COLOR_ON=GREEN, COLOR_OFF=DARK_GRAY)
-display1.init_row(X_ORG=8, Y_ORG=22, COL_INTV=6)
 
-display2 = LCD_font(screen)
-display2.init_col(BLOCK_SIZE=7, BLOCK_INTV=8, COLOR_ON=RED, COLOR_OFF=GRAY)
-display2.init_row(X_ORG=2, Y_ORG=18, COL_INTV=6)
-
-display3 = LCD_font(screen)
-display3.init_col(BLOCK_SIZE=4, BLOCK_INTV=4)
-display3.init_row(X_ORG=20, Y_ORG=66, COL_INTV=6)
-
-display4 = LCD_font(screen)
-display4.init_col(BLOCK_SIZE=4, BLOCK_INTV=4)
-display4.init_row(X_ORG=2, Y_ORG=76, COL_INTV=6)
-
-display5 = LCD_font(screen)
-display5.init_col(BLOCK_SIZE=9, BLOCK_INTV=9, COLOR_ON=(120, 200, 250), COLOR_OFF=GRAY)
-display5.init_row(X_ORG=8, Y_ORG=8, COL_INTV=6)
-
+lcd1 = LCD_font_styles
 
 running = True
 # infinite loop top ----
@@ -52,33 +30,22 @@ while running:
                 running = False
         if not running:
             break
-        # 「for count」のループから抜ける。whileループも抜ける。
-
-        display1.update_col(col=0, code=2 // (60 ** 3) )  # 4096の位
-        display1.update_col(col=1, code=4 // (60 ** 2))    # 256の位
-        display1.update_col(col=2, code=6 // 60)          # 16の位
-        display1.update_col(col=3, code=8 )                # 1の位
-#display1=真ん中の緑
-        display2.update_col(col=0  // (10 ** 4))   # 1000の位
-        display2.update_col(col=1  // (10 ** 3))   # 1000の位
-        display2.update_col(col=2  // (10 ** 2))   # 100の位
-        display2.update_col(col=3  // (10 ** 1))   # 10の位
-        display2.update_col(col=4  // (10 ** 0))   # 1の位 
-#display2=赤
-        display3(zfil=False, rjust=3)
-#display3=白上段
-        display4(zfil=True, rjust=16)
-#display4=白下段
         dt_now = datetime.now()
         time_now = (dt_now.hour * 10000
                     + dt_now.minute * 100
                     + dt_now.second)
-        display5(zfil=True, rjust=6, num=time_now, base=10)
-#display5=青
-        pygame.display.flip()  # update_col
-        clock.tick(20)  # FPS, Frame Per Second
-    screen.fill(DARK_GRAY)
-# infinit loop bottom ----
+        
+lcd1.update_col(col=0, code=dt_now.hour // 10)
+lcd1.update_col(col=1, code=dt_now.hour % 10)
+lcd1.update_col(col=2, code=10)
+lcd1.update_col(col=3, code=dt_now.minute // 10)
+lcd1.update_col(col=4, code=dt_now.minute % 10)
+lcd1.update_col(col=5, code=10)
+lcd1.update_col(col=6, code=dt_now.second // 10)
+lcd1.update_col(col=7, code=dt_now.second % 10)
+
+       
+ 
 
 pygame.quit()
 
